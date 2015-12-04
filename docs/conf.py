b68f3aa3939ba9c40352f196d9620c8655d59913
Christoph Paulik <cpaulik@gmail.com>
2015-12-04 12:24:45 +0100

    Update to pyscaffold 2.4.4

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pynetcf'
-copyright = u'2014, Christoph Paulik'
+copyright = u'2014, TU Wien'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -221,7 +221,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
     ('index', 'user_guide.tex', u'pynetcf Documentation',
-     u'Christoph Paulik', 'manual'),
+     u'TU Wien', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
