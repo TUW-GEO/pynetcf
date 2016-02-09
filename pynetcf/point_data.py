3181f60f2eb7781db1fadf8e230d17b2cda14abb
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2016-02-09 23:58:35 +0100

    add new class accessing multiple items

--- pynetcf/point_data.py
+++ pynetcf/point_data.py
@@ -83,11 +83,64 @@ class AccessItem(object):
                 self.name, dtype, dim, fill_value=fill_value,
                 zlib=zlib, complevel=complevel, chunksizes=chunksizes)
 
-        # for attr_name in attr:
-        #     attr_value = attr[attr_name]
-        #     self.obj.data.variables[name].setncattr(attr_name, attr_value)
-
         var[idx] = value
+
+
+class AccessMultipleItems(object):
+
+    def __init__(self, obj, names, **kwargs):
+        self.obj = obj
+        self.names = names
+        self.kwargs = kwargs
+
+    def __getitem__(self, item):
+
+        data = []
+        for name in self.names:
+            data.append(self.obj.data.variables[name][item])
+
+        return data
+
+    def __setitem__(self, idx, value):
+
+        for name in self.names:
+
+            if name in self.obj.data.variables.keys():
+                var = self.obj.data.variables[name]
+            else:
+                fill_value = None
+
+                if 'attr' in self.kwargs:
+                    if '_FillValue' in self.kwargs['attr']:
+                        fill_value = self.kwargs['attr'].pop('_FillValue')
+
+                if 'dtype' in self.kwargs:
+                    dtype = self.kwargs['dtype']
+                else:
+                    dtype = value.dtype
+
+                if 'zlib' in self.kwargs:
+                    zlib = self.kwargs['zlib']
+                else:
+                    zlib = self.obj.zlib
+
+                if 'complevel' in self.kwargs:
+                    complevel = self.kwargs['complevel']
+                else:
+                    complevel = self.obj.complevel
+
+                if 'chunksizes' in self.kwargs:
+                    chunksizes = self.kwargs['chunksizes']
+                else:
+                    chunksizes = None
+
+                dim = self.kwargs['dim']
+
+                var = self.obj.data.createVariable(
+                    name, dtype, dim, fill_value=fill_value, zlib=zlib,
+                    complevel=complevel, chunksizes=chunksizes)
+
+            var[idx] = value[name]
 
 
 class NcData(object):
@@ -119,7 +172,10 @@ class NcData(object):
                                     format=file_format)
 
         if self.mode == 'w':
-            self._create_dims(dims)
+            if dims is None:
+                raise ValueError("Dimensions not defined.")
+            else:
+                self._create_dims(dims)
 
     def _create_dims(self, dims):
         """
@@ -151,7 +207,6 @@ class NcData(object):
             Variable data.
         dim : tuple
             Variable dimensions.
-
         """
         if self.mode in ['w', 'r+', 'a']:
             self.io(name, **kwargs)[:] = data
@@ -162,8 +217,8 @@ class NcData(object):
 
         Parameters
         ----------
-        name : string
-            name of the variable
+        name : str
+            Name of the variable.
         """
         if self.mode in ['r', 'r+']:
             return self.io(name, **kwargs)[:]
@@ -171,6 +226,16 @@ class NcData(object):
     def io(self, name, **kwargs):
         """
         Description.
+
+        Parameters
+        ----------
+        name : str
+            Name of the variable.
+
+        Returns
+        -------
+        var : numpy.ndarray
+            Data stored in variable.
         """
         return AccessItem(self, name, **kwargs)
 
@@ -218,12 +283,10 @@ class PointData(NcData):
                  time_var='time', lat_var='lat', lon_var='lon', alt_var='alt',
                  unlim_chunksize=None, read_bulk=False, **kwargs):
 
-        super(PointData, self).__init__(filename, **kwargs)
-
-        self.n_obs = n_obs
-
-        # dimension names
         self.obs_dim_name = obs_dim_name
+
+        self.dims = {}
+        self.dims[self.obs_dim_name] = n_obs
 
         # location names
         self.loc_ids_name = loc_ids_name
@@ -241,210 +304,85 @@ class PointData(NcData):
         if unlim_chunksize is not None:
             self.unlim_chunksize = [unlim_chunksize]
 
-        # variable to track write operations
-        self.write_operations = 0
-        self.write_offset = None
+        super(PointData, self).__init__(filename, dims=self.dims, **kwargs)
 
         if self.mode == 'w':
-            self._init_dimensions()
             self._init_location_variables()
-            self._init_location_id_and_time()
-            self.global_attr['featureType'] = 'point'
-
-        self.read_bulk = read_bulk
-
-        # if read bulk is activated the arrays will be read into the
-        # local variables dict if it is not activated the data will be read
-        # from the netCDF variables
-        if not self.read_bulk:
-            self.variables = self.dataset.variables
-        else:
-            self.variables = {}
-
-    def _init_dimensions(self):
-        """
-        Initializes the dimensions.
-        """
-        self.create_dim(self.obs_dim_name, self.n_obs)
+            self.gattr['featureType'] = 'point'
 
     def _init_location_variables(self):
         """
         Initialize location information: longitude, latitude and altitude.
         """
-        self.write_var(self.lon_var, data=None, dim=self.obs_dim_name,
-                       attr={'standard_name': 'longitude',
-                             'long_name': 'location longitude',
-                             'units': 'degrees_east',
-                             'valid_range': (-180.0, 180.0)},
-                       dtype=np.float32)
+        self.write({self.lon_var: None}, dim=self.obs_dim_name,
+                   attr={'standard_name': 'longitude',
+                         'long_name': 'location longitude',
+                         'units': 'degrees_east',
+                         'valid_range': (-180.0, 180.0)},
+                   dtype=np.float32)
 
-        self.write_var(self.lat_var, data=None, dim=self.obs_dim_name,
-                       attr={'standard_name': 'latitude',
-                             'long_name': 'location latitude',
-                             'units': 'degrees_north',
-                             'valid_range': (-90.0, 90.0)},
-                       dtype=np.float32)
+        self.write({self.lat_var: None}, dim=self.obs_dim_name,
+                   attr={'standard_name': 'latitude',
+                         'long_name': 'location latitude',
+                         'units': 'degrees_north',
+                         'valid_range': (-90.0, 90.0)},
+                   dtype=np.float32)
 
-        self.write_var(self.alt_var, data=None, dim=self.obs_dim_name,
-                       attr={'standard_name': 'height',
-                             'long_name': 'vertical distance above the '
-                             'surface',
-                             'units': 'm',
-                             'positive': 'up',
-                             'axis': 'Z'},
-                       dtype=np.float32)
+        self.write({self.alt_var: None}, dim=self.obs_dim_name,
+                   attr={'standard_name': 'height',
+                         'long_name': 'vertical distance above the '
+                         'surface', 'units': 'm', 'positive': 'up',
+                         'axis': 'Z'},
+                   dtype=np.float32)
 
-    def _check_var(self, var):
-        """
-        Checks variable
+        self.write({self.time_var: None}, dim=self.obs_dim_name,
+                   attr={'standard_name': 'time'},
+                   dtype=np.float64)
 
-        Parameters
-        ----------
-        var : int, string or numpy.ndarray
-            Variable.
-
-        Returns
-        -------
-        var : numpy.ndarray
-            Variable.
-        """
-        if var is not None:
-            if type(var) != np.ndarray:
-                var = np.array([var])
-
-        # netCDF library can not handle arrays of length 1 that contain only a
-        # None value
-        if var.size == 1 and var[0] is None:
-            var = None
-
-        return var
-
-    def _add_observation(self, loc_id, time, lon, lat, alt=None, loc_descr=None):
-        """
-        Add a new observation to the data set.
-
-        Parameters
-        ----------
-        loc_id : int or numpy.ndarray
-            Location id of observation.
-        time : int or numpy.ndarray
-            Time of observation.
-        lon : float or numpy.ndarray
-            Longitudes of observation.
-        lat : float or numpy.ndarray
-            Longitudes of observation.
-        alt : float or numpy.ndarray
-            Altitude of observation.
-        loc_descr : string or numpy.ndarray
-            Location description.
-        """
-        loc_id = self._check_var(loc_id)
-        time = self._check_var(time)
-        lon = self._check_var(lon)
-        lat = self._check_var(lat)
-        alt = self._check_var(alt)
-
-        loc_ids_new = None
-        index = None
-
-        if time is not None:
-            self.dataset.variables[self.time_var][index] = time[loc_ids_new]
-
-        if lon is not None:
-            self.dataset.variables[self.lon_var][index] = lon[loc_ids_new]
-
-        if lat is not None:
-            self.dataset.variables[self.lat_var][index] = lat[loc_ids_new]
-
-        if alt is not None:
-            self.dataset.variables[self.alt_var][index] = alt[loc_ids_new]
-
-        if loc_descr is not None:
-            pass
-
-    def write(self, loc_id, data, dates, loc_descr=None, time=None, lon=None,
-              lat=None, alt=None, fill_values=None, attributes=None,
-              dates_direct=False):
+    def write(self, data, **kwargs):
         """
         Write.
 
         Parameters
         ----------
-
+        data : dict
+            Dictionary.
         """
-        try:
-            idx = self._get_loc_id_index(loc_id)
-        except IOError:
-            idx = self._add_location(loc_id, lon, lat, alt, loc_descr)
+        if self.mode in ['w', 'r+', 'a']:
+            self.io(data.keys(), **kwargs)[:] = data
 
-        # find out if attributes is a dict to be used for all variables or if
-        # there is a dictionary of attributes for each variable
-        unique_attr = False
-        if attributes is not None:
-            if sorted(data.keys()) == sorted(attributes.keys()):
-                unique_attr = True
-
-        for key in data:
-            if data[key].size != dates.size:
-                raise IOError("Timestamps and dataset {:} "
-                              "must have the same size".format(key))
-
-        for key in data:
-
-            internal_attributes = {'name': key,
-                                   'coordinates': 'time lat lon alt'}
-
-            if type(fill_values) == dict:
-                internal_attributes['_FillValue'] = fill_values[key]
-
-            if attributes is not None:
-                if unique_attr:
-                    variable_attributes = attributes[key]
-                else:
-                    variable_attributes = attributes
-
-                internal_attributes.update(variable_attributes)
-
-            if self.unlim_chunksize is None:
-                chunksizes = None
-            else:
-                chunksizes = [self.n_loc, self.unlim_chunksize[0]]
-
-            self.write_var(key, data=None, dim=(self.obs_dim_name),
-                           attr=internal_attributes,
-                           dtype=data[key].dtype, chunksizes=chunksizes)
-
-            if self.write_offset is None:
-                # find start of elements that are not yet filled with values
-                _slice_new = slice(self.length_before_extend, None, None)
-                masked = np.where(
-                    self.dataset.variables[key][idx, _slice_new].mask)[0]
-                # all indexes already filled
-                if len(masked) == 0:
-                    raise IOError("No free data slots available")
-                else:
-                    self.write_offset = np.min(
-                        masked) + self.length_before_extend
-
-            _slice = slice(self.write_offset, None, None)
-            # has to be reshaped to 2 dimensions because it is written
-            # into 2d variable otherwise netCDF library gets confused,
-            # might be a bug in netCDF?
-            self.dataset.variables[key][idx, _slice] = \
-                data[key].reshape(1, data[key].size)
-
-    def read(self, name):
+    def read(self, name, **kwargs):
         """
-        Read.
+        reads variable from netCDF file
 
         Parameters
         ----------
+        name : str or list of str
+            Name of the variable.
 
         Returns
         -------
-
+        var : numpy.ndarray
+            Data stored in variable.
         """
-        return self.read_var(name)
+        if self.mode in ['r', 'r+']:
+            return self.io(name, **kwargs)[:]
+
+    def io(self, name, **kwargs):
+        """
+        Description.
+
+        Parameters
+        ----------
+        name : str or list of str
+            Name of the variable.
+
+        Returns
+        -------
+        var : numpy.ndarray
+            Data stored in variable.
+        """
+        return AccessMultipleItems(self, name, **kwargs)
 
 
 import os
@@ -464,15 +402,31 @@ class NcDataTest(unittest.TestCase):
 
         dims = {'obs': 10}
 
-        with NcData(self.fn, dims, mode='w') as data:
+        with NcData(self.fn, dims, mode='w') as nc:
             var = np.arange(10)
-            data.write('myvar1', var, dim=('obs',))
-            data.write('myvar2', var, dim=('obs',))
-            data.io('myvar3', dim=('obs', ))[0:5] = np.arange(5)
+            nc.write('myvar1', var, dim=('obs',))
+            nc.write('myvar2', var, dim=('obs',))
+            nc.io('myvar3', dim=('obs', ))[0:5] = np.arange(5)
 
-        with NcData(self.fn) as data:
-            print data.io('myvar1')[0:4]
-            print data.io('myvar3')[:]
+        with NcData(self.fn) as nc:
+            print nc.io('myvar1')[0:4]
+            print nc.io('myvar3')[:]
+
+
+class NcPointDataTest(unittest.TestCase):
+
+    def setUp(self):
+        self.fn = os.path.join('/home', 'shahn', 'test.nc')
+
+    def tearDown(self):
+        pass
+        # os.remove(self.fn)
+
+    def test_read_write(self):
+
+        data = {"var1": np.arange(5)}
+        with PointData(self.fn, 5, mode='w') as nc:
+            nc.write(data)
 
 
 if __name__ == "__main__":
